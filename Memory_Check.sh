3e02af006561da891f594c4ecf72f5371420949f
juliustonacao <79636186+juliustonacao@users.noreply.github.com>
2021-02-27 04:42:35 +0800

    Update Memory_Check.sh

--- Memory_Check.sh
+++ Memory_Check.sh
@@ -1,53 +1,53 @@
 nano memory_check.sh
 # this will create the script file
 # you will be routed inside the memory_check.sh wherein you  will write the code
+# chmod u+x memory_check.sh to modify execution rights
 
-#!bin/bash
-#############################################################################################
-# Purpose: Memory Check
+#!/bin/bash
+# A script about memory alert
 # Author : JCLT
-#############################################################################################
 
-## Checking the total Memory and assigning variable
-free -mt | awk '/Mem:/{print $2 " MB Total Memory "}'
-TOTAL_MEMORY=$( free -mt | awk '/Mem:/{print $2 " MB Total Memory"}' )
+threshold1=60 # in percent for first reach point
+threshold2=90 # in percent for second reach point
+sudo hostname bpx.server.local
+host=`hostname -f` 
+alert=email@mine.com # Email that the alerts will be sent to
+logfile="/var/log/memory.log" #alerts are sent here
+sudo timedatectl set-timezone Asia/Manila #setting timezone to PHL
 
-## Checking how much memoy is used and assigning variable
-free -mt | awk '/Mem:/{print $3 " MB Total Memory "}'
-USED_MEMORY=$( free -mt | awk '/Mem:/{print $3 " MB Total Memory"}' )
+# Calculate real-time used memory and assigning variable REALMEM
 
-## Checking how much is the free memory and assigning variable
-free -mt | awk '/Mem:/{print $4 " MB Total Memory "}'
-FREE_MEMORY=$( free -mt | awk '/Mem:/{print $4 " MB Total Memory"}' )
+REALMEM=`free | awk 'FNR == 3 {print $4/($3+$4)*100-100}'| sed 's/-//g' |cut -d "." -f1`
 
-## Checking the RAM_USAGE percentage
-RAM_USAGE=$( free -mt | awk '/Mem:/{printf(" RAM USAGE %.2f\n "), $$3/$2*100}' )
+if [ "$REALMEM" -gt "$threshold1" ]
+then
 
-## Now we have set up four variables and we can simply call them out using echo command and $variable
-## The four variables are as follows : TOTAL_MEMORY, FREE_MEMORY, USED_MEMORY, RAM_USAGE. Please note that these are case sensitive
+date=`date`
+echo "The memory usage has reached $REALMEM% on $host" | mail -s "$host : High Memory Usage Alert" $alert
+echo "$date : The memory usage has reached $REALMEM% on $host." >> $logfile
 
-## CONDDITIONALS: If the used memory is <60% of the TOTAL_MEMORY, do nothing
-## exit code should be 0
-if [[ "$USED_MEMORY" -le  ]]
+##EDIT HERE, SEND EMAIL PART 
 
-exit 0
+if [ "$REALMEM" -gt "$threshold2" ]
+then
+
+#SUBJECT="ATTENTION: Memory Utilization is Critical on $(hostname) at $(date)"
+DATE="date +"%Y%m%d%T""$
+MESSAGE="/tmp/Mail.out"
+TO="email@mine.com"
+
+echo "Memory Current Usage is: $ramusage%" >> $MESSAGE
+  echo "" >> $MESSAGE
+  echo "------------------------------------------------------------------" >> $MESSAGE
+  echo "Top Memory Consuming Process Using top command" >> $MESSAGE
+  echo "------------------------------------------------------------------" >> $MESSAGE
+  echo "$(top -b -o +%MEM | head -n 20)" >> $MESSAGE
+  
+  mail -s "$DATE" "$TO" < $MESSAGE
+  rm /tmp/Mail.out
+  fi
+
+# Put your own urgency commands here
+
 fi
-
-## CONDDITIONALS: If the used memory is >= 60% of the TOTAL_MEMORY, trigger a WARNING
-## exit code should be 1
-
-exit 1
 fi
-
-
-## CONDDITIONALS: If the used memory is >= 90% of the TOTAL_MEMORY, trigger a CRITICAL WARNING 
-## List the top processes that consumes the memory and send an email to email@mine.com
-## format should be 20220225 06:00 Memory Check - Critical
-## exit code should be 2
-
-exit 2
-fi
-
-
-##command for top processes
-TOPPROCESS=$(ps -eo pid -eo pcpu -eo command | sort -k 2 -r | grep -v PID | head -n 1)
