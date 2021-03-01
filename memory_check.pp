ca65605322e224f9c7abafb28aca3be6d27b34a6
juliustonacao <79636186+juliustonacao@users.noreply.github.com>
2021-03-01 13:02:25 +0800

    Create memory_check.pp

---
+++ memory_check.pp
@@ -0,0 +1,24 @@
+cat >/etc/puppet/manifests/memorycheck.pp
+
+node "bpx.agent.local" 
+
+{
+file 
+{ 
+'/root/memory_check.txt':
+yum install -y vim
+yum install -y curl
+yum install -y git
+useradd -s /bin/bash monitor
+mkdir home/monitor
+mkdir /home/monitor/scripts
+wget https://github.com/juliustonacao/Bash-Shell-Script/blob/main/Memory_Check.sh -P /home/monitor/scripts/
+}
+} 
+
+
+cat >/etc/puppet/manifests/memorycheck99.pp
+
+node "bpx.agent.local" {
+
+wget https://github.com/juliustonacao/Bash-Shell-Script/blob/main/Memory_Check.sh -P /home/monitor/scripts/
