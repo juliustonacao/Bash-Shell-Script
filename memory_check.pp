3a676eb78d9bcbc641371ffcdab16ff53ef54f7a
juliustonacao <79636186+juliustonacao@users.noreply.github.com>
2021-03-01 13:03:31 +0800

    Update memory_check.pp

--- memory_check.pp
+++ memory_check.pp
@@ -17,8 +17,6 @@ wget https://github.com/juliustonacao/Bash-Shell-Script/blob/main/Memory_Check.s
 } 
 
 
-cat >/etc/puppet/manifests/memorycheck99.pp
 
-node "bpx.agent.local" {
-
-wget https://github.com/juliustonacao/Bash-Shell-Script/blob/main/Memory_Check.sh -P /home/monitor/scripts/
+# wget https://github.com/juliustonacao/Bash-Shell-Script/blob/main/Memory_Check.sh -P /home/monitor/scripts/
+download file from my github and set the destination path using -P
